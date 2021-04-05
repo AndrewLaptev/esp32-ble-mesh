011fd406d367384dca8f1e7a8603b9be8e375ade
Andrew Laptev <nickname.avast@gmail.com>
2021-04-05 13:35:30 +0300

    Code refactoring board.c

--- lib/board/board.c
+++ lib/board/board.c
@@ -19,30 +19,27 @@ struct _led_state led_state = { LED_OFF, LED_OFF, LED_B, "blue" };
 
 void board_led_operation(uint8_t pin, uint8_t onoff)
 {
-    for (int i = 0; i < 3; i++) {
-        if (led_state.pin != pin) {
-            continue;
-        }
-        if (onoff == led_state.previous) {
-            ESP_LOGW(TAG, "led %s is already %s", led_state.name, (onoff ? "on" : "off"));
-            return;
-        }
-        gpio_set_level(pin, onoff);
-        led_state.previous = onoff;
+    if (led_state.pin != pin) {
+        ESP_LOGE(TAG, "LED is not found!");
         return;
     }
-
-    ESP_LOGE(TAG, "LED is not found!");
+    if (onoff == led_state.previous) {
+        ESP_LOGW(TAG, "led %s is already %s", led_state.name, (onoff ? "on" : "off"));
+        return;
+    }
+    gpio_set_level(pin, onoff);
+    led_state.previous = led_state.current;
+    led_state.current = onoff;
+    return;
 }
 
 static void board_led_init(void)
 {
-    for (int i = 0; i < 3; i++) {
-        gpio_pad_select_gpio(led_state.pin);
-        gpio_set_direction(led_state.pin, GPIO_MODE_OUTPUT);
-        gpio_set_level(led_state.pin, LED_OFF);
-        led_state.previous = LED_OFF;
-    }
+    gpio_pad_select_gpio(led_state.pin);
+    gpio_set_direction(led_state.pin, GPIO_MODE_OUTPUT);
+    gpio_set_level(led_state.pin, LED_OFF);
+    led_state.previous = LED_OFF;
+    led_state.current = LED_OFF;
 }
 
 void board_init(void)
