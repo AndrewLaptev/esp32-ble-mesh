8a6de6948c40af11fc44afa29c62ff13716d63d1
Andrew Laptev <nickname.avast@gmail.com>
2021-04-29 03:20:23 +0300

    Add rssi_fade_pwm_onoff() and delete LEDC_PWM_LEVEL_DEF (blink_pwm dependency)

--- lib/rssi_ledc/rssi_ledc.c
+++ lib/rssi_ledc/rssi_ledc.c
@@ -19,48 +19,67 @@ ledc_channel_config_t ledc_channel[LEDC_TEST_CH_NUM] = {
     }
 };
 
-void rssi_fade_pwm_modes(short int channel_num, int *blink_pwm){
-    static short int mode_cur = 0;
-    if((*blink_pwm > 0) && (*blink_pwm < 100) && mode_cur != 1){
-        ledc_set_duty(ledc_channel[channel_num].speed_mode, ledc_channel[channel_num].channel, 0);
-        ledc_update_duty(ledc_channel[channel_num].speed_mode, ledc_channel[channel_num].channel);
-        ESP_LOGW("TEST", "mode OFF: %d", *blink_pwm);
-        mode_cur = 1;
-    }else if((*blink_pwm > 100) && (*blink_pwm < 1000) && mode_cur != 2){
-        ledc_set_fade_with_time(ledc_channel[channel_num].speed_mode,
-                ledc_channel[channel_num].channel, 500, LEDC_PWM_DELAY_FADE);
-        ledc_fade_start(ledc_channel[channel_num].speed_mode,
-                ledc_channel[channel_num].channel, LEDC_FADE_NO_WAIT);
-        ESP_LOGW("TEST", "mode LOW: %d", *blink_pwm);            
-        mode_cur = 2;
-    }else if((*blink_pwm > 1100) && (*blink_pwm < 3000) && mode_cur != 3){
-        ledc_set_fade_with_time(ledc_channel[channel_num].speed_mode,
-                ledc_channel[channel_num].channel, 2000, LEDC_PWM_DELAY_FADE);
-        ledc_fade_start(ledc_channel[channel_num].speed_mode,
-                ledc_channel[channel_num].channel, LEDC_FADE_NO_WAIT);
-        ESP_LOGW("TEST", "mode LOW-MEDIUM: %d", *blink_pwm);
-        mode_cur = 3;
-    }else if((*blink_pwm > 3100) && (*blink_pwm < 5000) && mode_cur != 4){
-        ledc_set_fade_with_time(ledc_channel[channel_num].speed_mode,
-                ledc_channel[channel_num].channel, 4000, LEDC_PWM_DELAY_FADE);
-        ledc_fade_start(ledc_channel[channel_num].speed_mode,
-                ledc_channel[channel_num].channel, LEDC_FADE_NO_WAIT);
-        ESP_LOGW("TEST", "mode MEDIUM: %d", *blink_pwm);
-        mode_cur = 4;
-    }else if((*blink_pwm > 5100) && (*blink_pwm < 8191) && mode_cur != 5){
-        ledc_set_fade_with_time(ledc_channel[channel_num].speed_mode,
-                ledc_channel[channel_num].channel, 8000, LEDC_PWM_DELAY_FADE);
-        ledc_fade_start(ledc_channel[channel_num].speed_mode,
-                ledc_channel[channel_num].channel, LEDC_FADE_NO_WAIT);
-        ESP_LOGW("TEST", "mode HIGH: %d", *blink_pwm);
-        mode_cur = 5;    
-    }
-}
-
 void rssi_ledc_pwm_fade_init(void){
     ledc_timer_config(&ledc_timer);
     for (short int channel_num = 0; channel_num < LEDC_TEST_CH_NUM; channel_num++) {
         ledc_channel_config(&ledc_channel[channel_num]);
     }
     ledc_fade_func_install(0);
+}
+
+void rssi_fade_pwm_modes(short int channel_num, int *rssi_mean){
+    static short int mode_cur = 0;
+    if((*rssi_mean > -91) && (*rssi_mean < -87) && mode_cur != 1){
+        ledc_set_duty(ledc_channel[channel_num].speed_mode, ledc_channel[channel_num].channel, 0);
+        ledc_update_duty(ledc_channel[channel_num].speed_mode, ledc_channel[channel_num].channel);
+        ESP_LOGW("TEST", "mode OFF: %d", *rssi_mean);
+        mode_cur = 1;
+    }else if((*rssi_mean > -85) && (*rssi_mean < -75) && mode_cur != 2){
+        ledc_set_fade_with_time(ledc_channel[channel_num].speed_mode,
+                ledc_channel[channel_num].channel, 500, LEDC_PWM_DELAY_FADE);
+        ledc_fade_start(ledc_channel[channel_num].speed_mode,
+                ledc_channel[channel_num].channel, LEDC_FADE_NO_WAIT);
+        ESP_LOGW("TEST", "mode LOW: %d", *rssi_mean);            
+        mode_cur = 2;
+    }else if((*rssi_mean > -70) && (*rssi_mean < -60) && mode_cur != 3){
+        ledc_set_fade_with_time(ledc_channel[channel_num].speed_mode,
+                ledc_channel[channel_num].channel, 2000, LEDC_PWM_DELAY_FADE);
+        ledc_fade_start(ledc_channel[channel_num].speed_mode,
+                ledc_channel[channel_num].channel, LEDC_FADE_NO_WAIT);
+        ESP_LOGW("TEST", "mode LOW-MEDIUM: %d", *rssi_mean);
+        mode_cur = 3;
+    }else if((*rssi_mean > -55) && (*rssi_mean < -45) && mode_cur != 4){
+        ledc_set_fade_with_time(ledc_channel[channel_num].speed_mode,
+                ledc_channel[channel_num].channel, 4000, LEDC_PWM_DELAY_FADE);
+        ledc_fade_start(ledc_channel[channel_num].speed_mode,
+                ledc_channel[channel_num].channel, LEDC_FADE_NO_WAIT);
+        ESP_LOGW("TEST", "mode MEDIUM: %d", *rssi_mean);
+        mode_cur = 4;
+    }else if((*rssi_mean > -40) && (*rssi_mean < -30) && mode_cur != 5){
+        ledc_set_fade_with_time(ledc_channel[channel_num].speed_mode,
+                ledc_channel[channel_num].channel, 8000, LEDC_PWM_DELAY_FADE);
+        ledc_fade_start(ledc_channel[channel_num].speed_mode,
+                ledc_channel[channel_num].channel, LEDC_FADE_NO_WAIT);
+        ESP_LOGW("TEST", "mode HIGH: %d", *rssi_mean);
+        mode_cur = 5;
+    }
+}
+
+void rssi_fade_pwm_onoff(short int channel_num, int *rssi_mean){
+    static short int onoff = 0;
+    if(*rssi_mean >= -50 && onoff == 0){
+        ledc_set_fade_with_time(ledc_channel[channel_num].speed_mode,
+                ledc_channel[channel_num].channel, 8191, LEDC_PWM_DELAY_FADE);
+        ledc_fade_start(ledc_channel[channel_num].speed_mode,
+                ledc_channel[channel_num].channel, LEDC_FADE_NO_WAIT);
+        ESP_LOGW("TEST", "LIGHT-ON: %d", *rssi_mean);
+        onoff = 1; 
+    }else if(*rssi_mean < -60 && onoff == 1){
+        ledc_set_fade_with_time(ledc_channel[channel_num].speed_mode,
+                ledc_channel[channel_num].channel, 0, LEDC_PWM_DELAY_FADE);
+        ledc_fade_start(ledc_channel[channel_num].speed_mode,
+                ledc_channel[channel_num].channel, LEDC_FADE_NO_WAIT);
+        ESP_LOGW("TEST", "LIGHT-OFF: %d", *rssi_mean);
+        onoff = 0;
+    }
 }
--- lib/rssi_ledc/rssi_ledc.h
+++ lib/rssi_ledc/rssi_ledc.h
@@ -11,11 +11,10 @@
 #define LEDC_HS_CH0_CHANNEL    LEDC_CHANNEL_0
 #define LEDC_TEST_CH_NUM       (1)
 #define LEDC_TIMER_FREQ        (5000)
-#define LEDC_PWM_LEVEL_DEF     (4000)
 #define LEDC_PWM_DELAY_FADE    (1000)
 
-void rssi_fade_pwm_modes(short int channel_num, int *blink_pwm);
-//void TaskRssiFadePwm(void *pvPatameters);
 void rssi_ledc_pwm_fade_init(void);
+void rssi_fade_pwm_modes(short int channel_num, int *blink_pwm);
+void rssi_fade_pwm_onoff(short int channel_num, int *blink_pwm);
 
 #endif
