e80694cab9500b1313d5bdd2f7fd7059c9e8809f
Andrew Laptev <nickname.avast@gmail.com>
2021-05-08 04:08:01 +0300

    Add rssi_distance_calculate()

--- lib/rssi_calc/rssi_calc.h
+++ lib/rssi_calc/rssi_calc.h
@@ -4,7 +4,8 @@
 #include <math.h>
 #include <stdbool.h>
 
-bool rssi_array_el_put(int *array, short int array_lengh, int elem); // if return 1 that array is full
+bool rssi_array_el_put(int *array, short int array_lengh, int elem, int *first_elem_ptr); // if return 1 that array is full
 int rssi_mean_calculate(int *array, short int array_lengh);
+double rssi_distance_calculate(int rssi_mom, float measure_rssi_dbm, float consider);
 
 #endif
